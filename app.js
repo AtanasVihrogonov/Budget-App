f5ab416c1a7d0db9b9da187a19b4516ec0988336
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-30 13:04:36 +0100

    adding eventListeners for input data

--- app.js
+++ app.js
@@ -14,13 +14,26 @@ const budgetController = (function() {
 // UI CONTROLLER
 const UIController = (function() {
 
-  return {
-    getInput: function() {
-      let type = document.querySelector('.add__type').value;
-      let description = document.querySelector('.add__description').value;
-      let value = document.querySelector('.add__value').value;
+  const DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
   }
 
+  return {
+    getinput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 
@@ -35,10 +48,25 @@ const UIController = (function() {
 
 //GLOBAL APP CONTROLLER
 const controller = (function(budgetCtrl, UICtrl) {
+  // function that store all event listeners
+  let setupEventListeners = function() {
+    let DOM = UICtrl.getDOMstrings();
 
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if(event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+
+  // function called when we add new item
   let ctrlAddItem = function() {
     // Get the field input data
-
+    let input = UICtrl.getinput();
+    console.log(input);
     // Add the item to the budget controller
 
     // Add new item to the UI
@@ -49,12 +77,12 @@ const controller = (function(budgetCtrl, UICtrl) {
     
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    if(event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  // For calling the setupEventListeners() we have to create public initialization function. To be a public needs to return it as object.
+  return {
+    init: function() {
+      console.log('started');
+      setupEventListeners();
     }
-  });
+  }
 
 })(budgetController, UIController);
