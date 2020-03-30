81074c8b6827421765f2ff3ca3865c30548cac4f
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-03-30 21:11:49 +0100

    Cleaning input Fields

--- app.js
+++ app.js
@@ -73,7 +73,9 @@ const UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list',
   };
 
   return {
@@ -85,11 +87,24 @@ const UIController = (function() {
       };
     },
     addListItem: function(obj, type) {
+      let html, newHtml;
       // Create HTML string with placeholder text
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
 
       // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
 
       // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
     },
     getDOMstrings: function() { // ==> return private DOMstrings to the public!
       return DOMstrings;
@@ -132,7 +147,7 @@ const controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetController.addItem(input.type, input.description, input.value);
 
     // Add new item to the UI
-
+    UICtrl.addListItem(newItem, input.type);
     // Calculate the budget
 
     // Display the budget on the UI
